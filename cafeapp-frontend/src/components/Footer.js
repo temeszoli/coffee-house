08843b92b111654ce42feab9f2da99d15ada76ea
Zoltan <temeszoli18@gmail.com>
2023-03-21 13:27:08 +0100

    Footer kiegészítése

--- cafeapp-frontend/src/components/Footer.js
+++ cafeapp-frontend/src/components/Footer.js
@@ -1,7 +1,17 @@
+import {BsGithub} from 'react-icons/bs';
+import {AiOutlineMail} from 'react-icons/ai';
+import {GrLinkedin} from 'react-icons/gr';
+import {Link} from 'react-router-dom';
+
 export default function Footer(){
     return(
         <footer>
-            &copy;2023 | <a href="/">Premium Coffee House</a>
+            <div>&copy;2023 | <a href="/">Premium Coffee House</a></div>
+            <p>Made by: Temesvári Zoltán</p>
+            <h1>
+                <Link to='https://github.com/temeszoli'><BsGithub /></Link>{' '}
+                <a href="mailto:temeszoli18@gmail.com"><AiOutlineMail /></a>{' '}
+                <Link to='https://www.linkedin.com/in/zoltán-temesvári-9635231a7/'><GrLinkedin /></Link></h1>
         </footer>
     );
 }
